13395467bb9a2e753ef330f5c7baee884fb74fc3
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-09-21 23:01:16 -0300

    refactor: updates scene code

--- src/js/scenes/MainScene.ts
+++ src/js/scenes/MainScene.ts
@@ -2,37 +2,36 @@ import CardDeck from "../objects/card/CardDeck";
 import Sprite = Phaser.GameObjects.Sprite;
 
 export default class MainScene extends Phaser.Scene {
+    public cardDeck : CardDeck = new CardDeck();
 
     constructor() {
         super({
             key: "MainScene",
         });
+
+        this.cardDeck.shuffle();
     }
 
-    public create(){
-        let { width, height } = this.sys.game.canvas;
-        console.log("create main")
+    public create(): void {
+        let {width, height} = this.sys.game.canvas;
 
-        let cardDeck : CardDeck = new CardDeck();
-        cardDeck.shuffle();
+        const limitDeck: number = 10;
 
-        const limitDeck : number = 10;
-
-        for (let i : number = 10; i > 0; i--) {
+        for (let i: number = 10; i > 0; i--) {
             this.add
-                .sprite((width/4) - (i + Phaser.Math.Between(40,50)),height/2 - 40, 'Deck')
+                .sprite((width / 4) - (i + Phaser.Math.Between(40, 50)), height / 2 - 40, 'Deck')
                 .setScale(0.3);
         }
 
-        const deck : Sprite = this.add
-            .sprite(width/4 - (limitDeck+25),height/2 - 35, 'Deck')
+        const deck: Sprite = this.add
+            .sprite(width / 4 - (limitDeck + 25), height / 2 - 35, 'Deck')
             .setScale(0.3)
             .setInteractive({
                 cursor: 'pointer'
             });
 
-        const floppyStack : Sprite = this.add
-            .sprite(width/2,height/2, cardDeck.cards[0].key)
+        const floppyStack: Sprite = this.add
+            .sprite(width / 2, height / 2, this.cardDeck.cards[0].key)
             .setScale(0.3);
     }
 }
