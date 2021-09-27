f91f866f8167a8c644d8596586cc73981f6e6fad
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-09-26 22:40:37 -0300

    feat: add players

--- src/game/Objetcs/Entities/Player/PlayerRoundIterator.ts
+++ src/game/Objetcs/Entities/Player/PlayerRoundIterator.ts
@@ -1,3 +1,13 @@
-export default class PlayerRoundIterator{
+import Player from "./Player";
 
+export default class PlayerRoundIterator{
+    private readonly _players : Array<Player> = new Array<Player>();
+
+    constructor(players: Array<Player>) {
+        this._players = players;
+    }
+
+    get players(): Array<Player> {
+        return this._players;
+    }
 }
